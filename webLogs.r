083d21351e701f0ef368a05ac521dff89a7df6a4
Sergio Ciruela <cirudinezidane@gmail.com>
2012-02-22 12:49:46 +0100

    Update webLogs.r

--- webLogs.r
+++ webLogs.r
@@ -13,10 +13,10 @@ table(df$status)
 reqs=as.data.frame(table(df$date))
 
 
-pdf("D:/webLogs/graph1.pdf)
+pdf("D:/webLogs/graph1.pdf")
 ggplot(data=reqs, aes(x=as.Date(Var1), y=Freq)) + geom_line() + xlab('Date') + ylab('Requests') + opts(title='Traffic to Site')
 dev.off()
 
-pdf("D:/webLogs/graph2.pdf)
+pdf("D:/webLogs/graph2.pdf")
 ggplot(data=df, aes(x=format(status))) + geom_bar() + xlab('Status') + ylab('Count') + opts(title='Status') 
 dev.off()
